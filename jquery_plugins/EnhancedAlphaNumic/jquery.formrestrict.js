e4d2c4f23904b45b61ebc0dcb87373c1bc57f8e5
Stephen <lunar.city.team@gmail.com>
2012-02-02 05:38:31 -0500

    remove .project

--- jquery_plugins/EnhancedAlphaNumic/jquery.formrestrict.js
+++ jquery_plugins/EnhancedAlphaNumic/jquery.formrestrict.js
@@ -10,7 +10,16 @@
 
     jQuery.fn.restrict = function(sanitizationFunc) {
         $(this).bind(inputEvents, function(e) {
+        	var cursor; 
+        	if($.browser.msie && parseInt($.browser.version, 10) <=8){
+        		if(e.which == 37 || e.which == 39)
+        			var cursor = getCaretPosition($(this));
+        	}
             $(this).val(sanitizationFunc($(this).val()));
+            if($.browser.msie && parseInt($.browser.version, 10) <=8){
+            	if(e.which == 37 || e.which == 39)
+            	setCaretPosition($(this), cursor);
+            }
         });
     };
 
@@ -25,3 +34,43 @@
         $(this).restrict(sanitize);
     }
 })(jQuery);
+
+function setCaretPosition(elem, caretPos) {
+    if(elem != null) {
+        if(elem.createTextRange) {
+            var range = elem.createTextRange();
+            range.move('character', caretPos);
+            range.select();
+        }
+        else {
+            if(elem.selectionStart) {
+                elem.focus();
+                elem.setSelectionRange(caretPos, caretPos);
+            }
+            else
+                elem.focus();
+        }
+    }
+}
+
+function GetCaretPosition (ctrl) {
+    var CaretPos = 0;
+
+    if (document.selection) {  // IE
+        ctrl.focus ();
+        var Sel = document.selection.createRange ();
+        var Sel2 = Sel.duplicate();
+        Sel2.moveToElementText(ctrl);
+        var CaretPos = -1;
+        while(Sel2.inRange(Sel))
+        {
+            Sel2.moveStart('character');
+            CaretPos++;
+        }
+    }
+
+    else if (ctrl.selectionStart || ctrl.selectionStart == '0') {  // Others
+        CaretPos = ctrl.selectionStart;
+    }
+    return (CaretPos);
+}
