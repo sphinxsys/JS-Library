d166dfd598777e4958f111894f9f802b380f6171
Stephen <lunar.city.team@gmail.com>
2012-02-15 08:19:33 -0500

    add new route js file

--- backbone/Example_1/js/main.js
+++ backbone/Example_1/js/main.js
@@ -7,15 +7,16 @@ require.config({
 		jQuery: '../../libs/jquery.min',
 		Underscore: '../../libs/Underscore-min',
 		Backbone:'../../libs/Backbone-min'
-	}	
+	}
+	//priority: ['jQuery']	
 });
 
 require([
-	//'app',    //Load our app module and pass it to our definition function
-	//'order!../libs/jquery.min',               //Some plugins have to be loaded in order due to there no AMD compliance, because there scripts are not "Module"
-	//'order!../libs/Underscore-min',
-	//'order!../libs/Backbone-min'
-	'app'
+	'app',    //Load our app module and pass it to our definition function
+	'order!../../libs/jquery.min',               //Some plugins have to be loaded in order due to there no AMD compliance, because there scripts are not "Module"
+	'order!../../libs/Underscore-min',
+	'order!../../libs/Backbone-min'
+
 
 ], function(App){
 	//The "app" dependency is passed in as "App"
